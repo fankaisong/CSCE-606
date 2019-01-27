58b809ca6957ec8788652f824c647ed0247084f0
fankaisong <kfan@tamu.edu>
2019-01-26 21:32:18 -0600

    modification

--- hw1/lib/ruby_intro.rb
+++ hw1/lib/ruby_intro.rb
@@ -5,7 +5,7 @@
 def sum arr
   # YOUR CODE HERE
   sum = 0
-  arr.each {|i| sum+=i}
+  arr.each {|i| sum+=i} #sum all numbers
   return sum
 end
 
@@ -16,7 +16,7 @@ def max_2_sum arr
 		return arr[0]
 		
 	end
-	return arr.sort.last(2)[0] + arr.sort.last(2)[1]
+	return arr.sort.last(2)[0] + arr.sort.last(2)[1] #sort the array, the largest will be in front
 
 
 end
@@ -25,9 +25,9 @@ def sum_to_n? arr, n
   	# YOUR CODE HERE
 
   	l = arr.length - 1
-	(0..l).each do |i|
+	(0..l).each do |i| #these two for loop find the two numbers summation 
 		(i+1..l).each do |j|
-			if arr[i]+arr[j] == n
+			if arr[i]+arr[j] == n #if the summation equals 'n'
     			return true
     		end
     	end
@@ -48,15 +48,15 @@ end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
-  s = s.downcase
+  s = s.downcase #lower the cases
 
-	if s[0]=~ /[a-z]/
+	if s[0]=~ /[a-z]/  #if amount a-z
 
-		if s[0] =~ /[aeiou]/ 
+		if s[0] =~ /[aeiou]/  #if amount a,e,i,o,u
 			return false
 		end
 
-	elsif s==''
+	elsif s=='' #if empty input
 		return false
 
 	else
@@ -70,20 +70,20 @@ end
 def binary_multiple_of_4? s
   # YOUR CODE HERE
 
-  	for i in 0..s.length-1 do
+  	for i in 0..s.length-1 do #this loop checks for the format is 0,1 or not
   		if not (s[i] =~ /[0-1]/)
   			return false
   		end
   	end
 
 
-	if s==''		
+	if s==''#if the input is empty		
 		return false
 
-	elsif s.to_i(2)%4 == 0
+	elsif s.to_i(2)%4 == 0 #return true if input can divide by 4
 		return true
 
-	else 
+	else #return false 
 		return false
 	end
 end
@@ -105,7 +105,7 @@ class BookInStock
 		@price
 	end
 
-	def isbn
+	def isbn #getter
 		@isbn
 	end
 
@@ -116,11 +116,11 @@ class BookInStock
 
 
 
-	def isbn=(isbn)
+	def isbn=(isbn)#setter
 		@isbn = isbn
 	end
 
-	def price_as_string
+	def price_as_string #print out the price
 		return "$" '%.2f' % (@price)
 
 	end
